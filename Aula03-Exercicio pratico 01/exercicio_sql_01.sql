646ac7b42220a7e78ae02e9e4767f331d81fbeb5
Erick <erick.2210478300057@faeterj-rio.edu.br>
2025-04-04 20:28:56 -0300

    atualizando arquivos

--- Aula03-Exercicio pratico 01/exercicio_sql_01.sql
+++ Aula03-Exercicio pratico 01/exercicio_sql_01.sql
@@ -1,34 +1,34 @@
-1. Liste todos os alunos matriculados no curso de "Banco de Dados".
+-- 1. Liste todos os alunos matriculados no curso de "Banco de Dados".
 SELECT a.nome, a.email, a.aluno_id
 FROM aluno a
 JOIN matricula m ON a.aluno_id = m.aluno_id
 JOIN curso c ON m.curso_id = c.curso_id
 where c.titulo = 'Banco de Dados';
 
-2. Liste todos os cursos com carga horária maior que 40 horas.
+-- 2. Liste todos os cursos com carga horária maior que 40 horas.
 SELECT a.titulo, a.curso_id
 FROM curso a
 WHERE a.carga_horaria > 40;
 
-3. Liste os alunos que ainda não receberam nota.
+-- 3. Liste os alunos que ainda não receberam nota.
 SELECT a.aluno_id, a.nome, a.email
 FROM aluno a
 JOIN matricula m ON a.aluno_id = m.aluno_id
 WHERE m.nota is null;
 
-4. Liste as matrículas realizadas depois do dia 01/01/2024.
+-- 4. Liste as matrículas realizadas depois do dia 01/01/2024.
 SELECT *
 FROM matricula m
 WHERE m.data_matricula > '01-01-2024';
 
-5. Mostre os cursos com carga horária entre 30 e 60 horas.
+-- 5. Mostre os cursos com carga horária entre 30 e 60 horas.
 SELECT a.titulo, a.curso_id
 FROM curso a
 WHERE a.carga_horaria BETWEEN 30 and 60
 
-6. Liste os alunos com e-mails do domínio @gmail.com
+-- 6. Liste os alunos com e-mails do domínio @gmail.com
 SELECT a.email
 FROM aluno a
 WHERE a.email like '%@gmail.com%'
 
-7.Liste o nome do aluno, título do curso e data da matrícula.
+-- 7.Liste o nome do aluno, título do curso e data da matrícula.
